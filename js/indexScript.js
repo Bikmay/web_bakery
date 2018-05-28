b267d3f235614a8bf515b8123e4fb2502b232cc9
artyom <artyom.ivliev@gmail.com>
2018-05-28 12:44:36 +0400

    3

--- js/indexScript.js
+++ js/indexScript.js
@@ -5,9 +5,3 @@
 
     });
 
-    $('h4').next().hide();
-
-    $('h4').click(function(){
-        $(this).next().slideToggle();
-        $('h4').not(this).next().stop(true,true).slideUp();
-    });
--- js/sec-index-script.js
+++ js/sec-index-script.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-    $('#accordion').find('h2').click(function(){
-        $(this).next().stop().slideToggle();
-    }).next().stop().hide();
+$('h4').next().hide();
 
+$('h4').click(function(){
+    $(this).next().slideToggle();
+    $('h4').not(this).next().stop(true,true).slideUp();
 });
