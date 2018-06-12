ee069d305cd5e02cf1a1757191acce2b1a8a2663
Bikmay <artyom.ivliev@gmail.com>
2018-06-13 01:23:58 +0400

    2

---
+++ js/g_js.js
@@ -0,0 +1,23 @@
+
+    var a = document.getElementById("b_clear");
+
+    a.onmouseover = function () {
+        this.setAttribute("style","background-color:#C8C8C8; margin-left: 0.08vw");
+
+    };
+
+    a.onmouseout=function () {
+        this.setAttribute("style","background-color:white; margin-left: 0.08vw");
+    };
+
+    var b = document.getElementById("b_create");
+
+    b.onmouseover = function () {
+        this.setAttribute("style","background-color:#C8C8C8; ");
+
+    };
+
+    b.onmouseout=function () {
+        this.setAttribute("style","background-color:white;");
+    };
+
